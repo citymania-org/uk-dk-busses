1d9a044c6bdde99499c552f7ad573f83a4a7a2b4
BL97 <98127896+BL97@users.noreply.github.com>
2022-03-05 03:48:27 +0000

    Update generate.py

--- generate.py
+++ generate.py
@@ -7,8 +7,8 @@ import lib
 g = grf.NewGRF(
     b'CMBB',
     8,
-    'Brilliant British Busses',
-    'Brilliant British Busses',
+    'Robs British Busses',
+    'Robs British Busses',
 )
 g.strings = lib.StringManager()
 RoadVehicle = g.bind(lib.RoadVehicle)
@@ -26,20 +26,20 @@ def tmpl_rv(x, y, func):
     ]
 
 
-optare_vectra_arriva_png = grf.ImageFile('sprites/UK 1991 Optare Vecta (Arriva).png', colourkey=(0, 0, 255))
-optare_vectra_tnd_png = grf.ImageFile('sprites/UK 1991 Optare Vecta (Tees And District).png', colourkey=(0, 0, 255))
+MAN_11190_OpVecta_1_png = grf.ImageFile('sprites/UK_1991_MAN_11_190_Optare_Vecta_(Arriva)_32bpp.png')
+MAN_11190_OpVecta_2_png = grf.ImageFile('sprites/UK_1991_MAN_11_190_Optare_Vecta_(Tees_And_District_Late)_32bpp.png')
 
 RoadVehicle(
     id=4,
-    name='Optare Vecta',
+    name='MAN 11.190 Optare Vecta',
     liveries = [
         {
             'name': ' (Arriva)',
-            'sprites': tmpl_rv(0, 0, lambda *args, **kw: grf.FileSprite(optare_vectra_arriva_png, *args, **kw, bpp=24)),
+            'sprites': tmpl_rv(0, 0, lambda *args, **kw: grf.FileSprite(MAN_11190_OpVecta_1_png, *args, **kw, bpp=32)),
         },
         {
             'name': ' (Tees and District)',
-            'sprites': tmpl_rv(0, 0, lambda *args, **kw: grf.FileSprite(optare_vectra_tnd_png, *args, **kw, bpp=24)),
+            'sprites': tmpl_rv(0, 0, lambda *args, **kw: grf.FileSprite(MAN_11190_OpVecta_2_png, *args, **kw, bpp=32)),
         },
     ],
     max_speed=lib.kmhishph(104),
@@ -54,29 +54,50 @@ RoadVehicle(
     cost_factor=246,
     refittable_cargo_types=1,
     additional_text=lib.fake_info_text({
-        'Manufacturer': 'Optare',
+        'Manufacturer': 'MAN',
     }),
 )
 
 
-plaxton_pointer_arriva_png = grf.ImageFile('sprites/UK 1996 Dennis Dart Plaxton Pointer 2 (Arriva).png', colourkey=(0, 0, 255))
+DenDrt_PP_1_png = grf.ImageFile('sprites/UK_1991_Dennis_Dart_Plaxton_Pointer_(Arriva)_32bpp.png')
+DenDrt_PP_2_png = grf.ImageFile('sprites/UK_1991_Dennis_Dart_Plaxton_Pointer_(East_Yorkshire)_32bpp.png')
+DenDrt_PP_3_png = grf.ImageFile('sprites/UK_1991_Dennis_Dart_Plaxton_Pointer_(First)_32bpp.png')
+DenDrt_PP_4_png = grf.ImageFile('sprites/UK_1991_Dennis_Dart_Plaxton_Pointer_(Go_Ahead_London)_32bpp.png')
+DenDrt_PP_5_png = grf.ImageFile('sprites/UK_1991_Dennis_Dart_Plaxton_Pointer_(Metroline)_32bpp.png')
+DenDrt_PP_6_png = grf.ImageFile('sprites/UK_1991_Dennis_Dart_Plaxton_Pointer_(Stagecoach)_32bpp.png')
 
 RoadVehicle(
     id=5,
-    name='Dennis Dart Plaxton Pointer 2',
+    name='Dennis Dart Plaxton Pointer',
     liveries = [
         {
             'name': ' (Arriva)',
-            'sprites': tmpl_rv(0, 0, lambda *args, **kw: grf.FileSprite(plaxton_pointer_arriva_png, *args, **kw, bpp=24)),
+            'sprites': tmpl_rv(0, 0, lambda *args, **kw: grf.FileSprite(DenDrt_PP_1_png, *args, **kw, bpp=32)),
         },
         {
-            'name': ' (Test)',
-            'sprites': tmpl_rv(0, 0, lambda *args, **kw: grf.FileSprite(plaxton_pointer_arriva_png, *args, **kw, bpp=24)),
+            'name': ' (East Yorkshire)',
+            'sprites': tmpl_rv(0, 0, lambda *args, **kw: grf.FileSprite(DenDrt_PP_2_png, *args, **kw, bpp=32)),
+        },
+        {
+            'name': ' (First)',
+            'sprites': tmpl_rv(0, 0, lambda *args, **kw: grf.FileSprite(DenDrt_PP_3_png, *args, **kw, bpp=32)),
+        },
+        {
+            'name': ' (Go Ahead London)',
+            'sprites': tmpl_rv(0, 0, lambda *args, **kw: grf.FileSprite(DenDrt_PP_4_png, *args, **kw, bpp=32)),
+        },
+        {
+            'name': ' (Metroline)',
+            'sprites': tmpl_rv(0, 0, lambda *args, **kw: grf.FileSprite(DenDrt_PP_5_png, *args, **kw, bpp=32)),
+        },
+        {
+            'name': ' (Stagecoach)',
+            'sprites': tmpl_rv(0, 0, lambda *args, **kw: grf.FileSprite(DenDrt_PP_6_png, *args, **kw, bpp=32)),
         },
     ],
     max_speed=lib.kmhishph(104),
     power=255,
-    introduction_date=date(1996, 1, 1),
+    introduction_date=date(1991, 1, 1),
     vehicle_life=8,
     model_life=144,
     climates_available=grf.ALL_CLIMATES,
@@ -103,19 +124,20 @@ def tmpl_rv_vox(x, y, func):
         func(x + 208, y, 26, 20, xofs= -6, yofs=-10),
     ]
 
-alx1000_arriva_png = grf.ImageFile('sprites/UK_1997_Mercedes_Benz_Vario_Alexander_ALX100_(Arriva).png')
+MB_VA_ALX100_1_png = grf.ImageFile('sprites/UK_1997_Mercedes-Benz_Vario_Alexander_ALX100_(Arriva)_32bpp.png')
+MB_VA_ALX100_2_png = grf.ImageFile('sprites/UK_1997_Mercedes-Benz_Vario_Alexander_ALX100_(First)_32bpp.png')
 
 RoadVehicle(
     id=6,
-    name='Alexander ALX1000',
+    name='Mercedes-Benz Vario Alexander ALX100',
     liveries = [
         {
             'name': ' (Arriva)',
-            'sprites': tmpl_rv_vox(0, 0, lambda *args, **kw: grf.FileSprite(alx1000_arriva_png, *args, **kw, bpp=32)),
+            'sprites': tmpl_rv_vox(0, 0, lambda *args, **kw: grf.FileSprite(MB_VA_ALX100_1_png, *args, **kw, bpp=32)),
         },
         {
-            'name': ' (Arriva)',
-            'sprites': tmpl_rv_vox(0, 0, lambda *args, **kw: grf.FileSprite(alx1000_arriva_png, *args, **kw, bpp=32)),
+            'name': ' (First)',
+            'sprites': tmpl_rv_vox(0, 0, lambda *args, **kw: grf.FileSprite(MB_VA_ALX100_2_png, *args, **kw, bpp=32)),
         },
     ],
     max_speed=lib.kmhishph(104),
@@ -130,8 +152,8 @@ RoadVehicle(
     cost_factor=246,
     refittable_cargo_types=1,
     additional_text=lib.fake_info_text({
-        'Manufacturer': 'Alexander',
-        'Operator': 'Arriva',
+        'Manufacturer': 'Mercedes-Benz',
+        'Operators': 'Arriva, First',
     }),
 )
 
